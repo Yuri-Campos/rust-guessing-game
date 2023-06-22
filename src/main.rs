56f70f2d43885fffa2e5a81ff8c4d72611a5ca5e
Yuri Campos <ygsoares20@gmail.com>
2023-06-22 03:39:38 -0300

    now with a victory case to break the loop

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
     match guess.cmp(&secret_number){
         Ordering::Less => println!("Too Small!"),
         Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!"),
+        Ordering::Equal => {println!("You Win!"); break},
     }
 
     }
