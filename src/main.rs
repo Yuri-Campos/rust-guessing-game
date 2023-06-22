ff40e4d78806e79f2b62966f79f8b71566b36fba
Yuri Campos <ygsoares20@gmail.com>
2023-06-22 03:28:12 -0300

    define the random number using the rand dependency

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use std::io;
+use::rand::Rng;
 
 fn main() {
     println!("Guess the Number!");
-
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is {}", secret_number);
     println!("Input Your Guess.");
+    
 
     let mut guess = String::new();
 
